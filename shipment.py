6dc301de6a9110dd63c2ae4d8bb9eefef7ddce2b
Carlos G?lvez <carlos.galvez@nan-tic.com>
2019-03-21 15:29:53 +0100

    Fix shipment_external_sequence. #037315

--- shipment.py
+++ shipment.py
@@ -28,7 +28,7 @@ class Configuration:
     def multivalue_model(cls, field):
         pool = Pool()
         if field in ['shipment_external_sequence']:
-            return pool.get('stock.configuration.shipment_external_sequence')
+            return pool.get('stock.configuration.sequence')
         return super(Configuration, cls).multivalue_model(field)
 
 
