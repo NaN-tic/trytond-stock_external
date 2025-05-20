22c4e02041c6be014839411b656bbc112dfb7772
Raimon Esteve <raimonesteve@gmail.com>
2025-05-20 10:57:50 +0200

    Removing deprecated args from setup

--- setup.py
+++ setup.py
@@ -119,8 +119,3 @@ setup(name='%s_%s' % (PREFIX, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
-    use_2to3=True,
-    convert_2to3_doctests=[
-        'tests/scenario_stock_external_shipment.rst',
-        ]
-    )
